4d29e313ee72c98767190949c723965f495d2eeb
Léo <leoclipet26@gmail.com>
2025-03-19 23:06:13 +0100

    Added print to MovementResult struct

--- TP_Math_Cpp/StructHeader.h
+++ TP_Math_Cpp/StructHeader.h
@@ -9,4 +9,14 @@ struct MovementResult
     FVector3 newV;
     FVector3 newTeta;
     FVector3 newTetap;
+
+    void print() const
+    {
+        std::cout << "MovementResult" << '\n';
+        std::cout << "newW: " << newW.ToString() << '\n';
+        std::cout << "newG: " << newG.ToString() << '\n';
+        std::cout << "newV: " << newV.ToString() << '\n';
+        std::cout << "newTeta: " << newTeta.ToString() << '\n';
+        std::cout << "newTetap: " << newTetap.ToString() << '\n';
+    }
 };
